2131c0aee6d8386c0bd2ebf80f425e5005217d05
Carlos Alejandro <alexco_99@hotmail.com>
2024-03-06 18:39:35 -0600

    +++

--- Assets/Alexco/Scripts/PlayerMovement.cs
+++ Assets/Alexco/Scripts/PlayerMovement.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     public float moveSpeed = 5f; // Velocidad de movimiento del jugador
+    public GameObject bulletPrefab; // Prefab de la bala
 
     private Rigidbody2D rb; // Referencia al componente Rigidbody2D
 
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>(); // Obtener el componente Rigidbody2D del jugador
     }
 
-    void Update()
+    private void Update()
     {
+        if (!IsLocalPlayer || !IsOwner)
+            return;
+
         // Obtener la entrada del eje horizontal (izquierda/derecha)
         float moveInputX = Input.GetAxisRaw("Horizontal");
 
@@ -25,5 +30,22 @@ public class PlayerMovement : MonoBehaviour
 
         // Asignar la velocidad al Rigidbody para mover al jugador
         rb.velocity = new Vector2(moveVelocityX, moveVelocityY);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Instanciar una nueva bala en la posición del jugador
+            SpawnBulletServerRpc();
+        }
+    }
+
+    [ServerRpc]
+    private void SpawnBulletServerRpc()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        NetworkObject bulletNetworkObject = bullet.GetComponent<NetworkObject>();
+        if (bulletNetworkObject != null)
+        {
+            bulletNetworkObject.Spawn();
+        }
     }
 }
